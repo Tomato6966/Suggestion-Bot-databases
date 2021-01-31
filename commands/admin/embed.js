5f0b8d5fbf86f038dd9e0b7bbdbbb50c030a05fd
Tomato#6966 <68145571+Tomato6966@users.noreply.github.com>
2021-01-31 11:11:11 +0100

    Update embed.js

--- commands/admin/embed.js
+++ commands/admin/embed.js
@@ -9,8 +9,16 @@ module.exports = {
   run: async (client, message, args) => {
 	if (!message.member.hasPermission("ADMINISTRATOR"))
 	return message.reply(`You are not allowed to run this command`)
-	const argsneu = message.content.slice(5+config.PREFIX.length).split("+");
-	if(!argsneu) return message.reply("Useage: `embed <Title> + <Description>`");
+	let prefix = client.settings.get(message.guild.id, `prefix`);
+	const argsneu = message.content.slice(5 + prefix.length).split("+");
+	if(!argsneu || !args[0]) {
+		message.channel.send(new MessageEmbed()
+		.setColor("#ff712e").setFooter(client.user.username, config.AVATARURL)
+		.setTimestamp()
+		.setTitle("THIS IS THE `TITLE`")
+		.setDescription("THIS IS THE `DESCRIPTION`"))
+		return message.reply("Useage: `embed <Title> + <Description>`");
+	}
 	const TITEL = argsneu[0];
 	const BESCHREIBUNG = argsneu.slice(1).join(" ");
 	const embed = new MessageEmbed()
